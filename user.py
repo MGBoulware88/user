32be3db6aad5bcf34d668fca9e60341f030c8ced
Gray-Desktop <gray.boulware@gmail.com>
2023-03-05 18:01:28 -0600

    chained methogs

--- user.py
+++ user.py
@@ -14,42 +14,27 @@ class User:
         print(self.age)
         print(self.is_rewards_member)
         print(self.gold_card_points)
+        return self
 
     def enroll(self):
         if self.is_rewards_member:
             print("User already a member.")
-            return False
-        self.is_rewards_member = True
-        self.gold_card_points = 200
-        return True
+        else:
+            self.is_rewards_member = True
+            self.gold_card_points = 200
+        return self
 
     def spend_points(self,amount):
         if self.gold_card_points >= amount:
             self.gold_card_points-=amount
         else: print("Your balance is too low.")
+        return self
 
 gray = User('Gray','Boulware','gray@gmail.com',34)
-gray.enroll()
-gray.spend_points(50)
-
+gray.enroll().spend_points(50).display_info().enroll()
 
 rick = User('Rick','Sanchez','rick@rick.com',67)
-rick.enroll()
-rick.spend_points(80)
+rick.enroll().spend_points(80).display_info()
 
 morty = User('Morty','Smith','morty@getsome.com',15)
-
-
-
-
-gray.display_info()
-print('')
-morty.display_info()
-print('')
-morty.display_info()
-
-#BONUS
-
-gray.enroll()
-
-morty.spend_points(40)
+morty.display_info().spend_points(40)
